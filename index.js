01ffadd17764a4794529ef924b4ac4fcb7d6edb3
Vash <mrbats1234@gmail.com>
2020-04-09 13:53:56 +0200

    Removed token

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const token = "Njk3NzI5MTYxMTA3NjAzNDk2.Xo7hBQ.4Q1EWEepQYDb68deJhP_IfnjkPs";
+const token = ""; // enter your own token here;
 
+// on ready log to console
 client.on("ready", () => {
   console.log("Client ready");
 });
 
+// the word to look for
 let word = "play";
 
+// on message send check if word matches
 client.on("message", (msg) => {
   if (msg.content.toLocaleLowerCase().match(/(^|\W)play($|\W)/i)) {
     console.log(msg.author.username);
     msg.delete();
 
     let sendMsg = msg.content.replace("play", "paly");
-    msg.reply(" " + sendMsg);
+    msg.channel.send(msg.author.username + ": " + sendMsg);
   }
 });
 
